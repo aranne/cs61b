f0cf814fd2e1a70ad0dac246fa9c80f8f6449d49
aranne <fuweiran816@gmail.com>
2019-07-18 16:48:19 +0800

    OffByN optimized

--- proj1b/TestOffByN.java
+++ proj1b/TestOffByN.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class TestOffByN {
+    int n = 5;
+    CharacterComparator cc = new OffByN(n);
+
+    @Test
+    public void testEqualChars() {
+        assertTrue(cc.equalChars('a', 'f'));
+
+        assertTrue(cc.equalChars('f', 'a'));
+
+        assertFalse(cc.equalChars('f', 'h'));
+    }
 }
