b64b49e22e4097328098204f414c05c3895f7e77
aranne <fuweiran816@gmail.com>
2019-07-18 14:30:53 +0800

    Deque interface

--- proj1b/Deque.java
+++ proj1b/Deque.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+public interface Deque<Item> {
+    /** Adds an item of type T to the front of the deque. */
+    public void addFirst(Item item);
 
-public class Deque {
+    /** Adds an item of type T to the back of the deque. */
+    public void addLast(Item item);
+
+    /** Returns true if deque is empty, false otherwise. */
+    public boolean isEmpty();
+
+    /** Returns the number of items in the deque. */
+    public int size();
+
+    /** Prints the items in the deque from first to last. */
+    public void printDeque();
+
+    /**
+     * Removes and returns the item at the front of the deque.
+     * If no such item exists, returns null.
+     */
+    public Item removeFirst();
+
+    /**
+     * Removes and returns the item at the back of the deque.
+     * If no such item exists, returns null.
+     */
+    public Item removeLast();
+
+    /**
+     * Get the item at the given index.
+     * If no such item exists, returns null.
+     */
+    public Item get(int index);
 }
