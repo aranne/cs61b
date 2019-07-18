bbb6552089612adee7e8f7ffa50913dda6c38c27
aranne <fuweiran816@gmail.com>
2019-07-18 14:31:11 +0800

    palindrome

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
-
 public class Palindrome {
+
+    /**
+     * Given a String, returns a Deque where the characters appear
+     * in the same order as in the String.
+     */
+    public Deque<Character> wordToDeque(String word) {
+        Deque<Character> d = new ArrayDeque<>();
+        for (int i = 0; i < word.length(); i += 1) {
+            d.addLast(word.charAt(i));
+        }
+        return d;
+    }
+
+    /** Returns true if the given word is a palindrome, false otherwise. */
+    public boolean isPalindrome(String word) {
+        Deque<Character> d = wordToDeque(word);
+        if (d.isEmpty()) {
+            return true;
+        } else {
+            int first = 0;
+            int last = d.size() - 1;
+            while (first <= last) {
+                if (d.get(first) != d.get(last)) {
+                    return false;
+                }
+                first += 1;
+                last -= 1;
+            }
+            return true;
+        }
+    }
 }
--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -2,7 +2,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestPalindrome {
-    /*// You must use this palindrome, and not instantiate
+    // You must use this palindrome, and not instantiate
     // new Palindromes, or the autograder might be upset.
     static Palindrome palindrome = new Palindrome();
 
@@ -13,6 +13,19 @@ public class TestPalindrome {
         for (int i = 0; i < "persiflage".length(); i++) {
             actual += d.removeFirst();
         }
+        // Test two strings equal, other than test two Deque equal.
+        // Because Deque doesn't have equal() method.
         assertEquals("persiflage", actual);
-    } Uncomment this class once you've created your Palindrome class. */
+    }
+
+    @Test
+    public void testIsPalindrome() {
+        assertFalse(palindrome.isPalindrome("parse"));
+
+        assertTrue(palindrome.isPalindrome("noon"));
+
+        assertTrue(palindrome.isPalindrome(""));
+
+        assertTrue(palindrome.isPalindrome("a"));
+    }
 }
