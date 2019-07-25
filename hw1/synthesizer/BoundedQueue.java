f0715fce746da384489ee47440add93b5aff5eb3
aranne <fuweiran816@gmail.com>
2019-07-25 15:22:31 +0800

    add two default method in BoundedQueue interface

--- hw1/synthesizer/BoundedQueue.java
+++ hw1/synthesizer/BoundedQueue.java
@@ -15,4 +15,14 @@ public interface BoundedQueue<T> {
 
     /** Returns (but not deletes) item from the front of the queue. */
     T peek();
+
+    /** Returns true if the buffer is empty. */
+    default boolean isEmpty() {
+        return fillCount() == 0;
+    }
+
+    /** Returns true if the buffer is full. */
+    default boolean isFull() {
+        return fillCount() == capacity();
+    }
 }
