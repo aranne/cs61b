3ba11ed40c58277545c4b01bc74b34a9abde114d
aranne <fuweiran816@gmail.com>
2019-08-06 16:11:29 -0400

    finished class percolationStats

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,58 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 
+public class PercolationStats {
+    private Percolation p;
+    private double[] data;
+    private int times;
+
+    /** Perform T independent experiments on an N-by-N grid. */
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException();
+        }
+        data = new double[T];
+        times = T;
+        // Experiment T times.
+        for (int i = 0; i < T; i += 1) {
+            p = pf.make(N);
+            while (!p.percolates()) {
+                int row = StdRandom.uniform(0, N);
+                int col = StdRandom.uniform(0, N);
+                p.open(row, col);
+            }
+            data[i] = (double) p.numberOfOpenSites() / (N * N);
+        }
+    }
+
+    /** Returns the sample mean of percolation threshold. */
+    public double mean() {
+        return StdStats.mean(data);
+    }
+
+    /** Returns the sample standard deviation of percolation threshold. */
+    public double stddev() {
+        return StdStats.stddev(data);
+    }
+
+    /** Returns the low endpoint of 95% confidence interval. */
+    public double confidenceLow() {
+        return mean() - 1.96 * Math.sqrt(stddev() / times);
+    }
+
+    /** Returns the high endpoint of 95% confidence interval. */
+    public double confidenceHigh() {
+        return mean() + 1.96 * Math.sqrt(stddev() / times);
+    }
+
+    public static void main(String[] args) {
+        PercolationFactory pf = new PercolationFactory();
+        PercolationStats ps = new PercolationStats(5, 40, pf);
+        System.out.println(ps.mean());
+        System.out.println(ps.stddev());
+        System.out.println(ps.confidenceLow());
+        System.out.println(ps.confidenceHigh());
+    }
 }
