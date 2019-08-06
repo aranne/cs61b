70432cd53e9ec4136fe2f2950930352833d4bfb4
aranne <fuweiran816@gmail.com>
2019-08-06 15:12:10 -0400

    finished backwash

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -6,6 +6,7 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class Percolation {
     private boolean[][] table;
     private WeightedQuickUnionUF ufTable;
+    private WeightedQuickUnionUF ufTopOnly;
     private int size;
     private int openNumber;
 
@@ -14,12 +15,8 @@ public class Percolation {
         if (N <= 0) {
             throw new IllegalArgumentException();
         }
+        // its default value will be false.
         table = new boolean[N][N];
-        for (boolean[] bs : table) {     // All sites are blocked initially.
-            for (boolean b : bs) {
-                b = false;
-            }
-        }
         // Create a UnionFind to track union situations.
         ufTable = new WeightedQuickUnionUF(N * N + 2);
         /* (N*N) site means the virtual top site,
@@ -34,6 +31,13 @@ public class Percolation {
         for (int i = N * N - N; i < N * N; i += 1) {
             ufTable.union(i, N * N + 1);
         }
+        ufTopOnly = new WeightedQuickUnionUF(N * N + 1);
+        /* (N*N) site means the virtual top site,
+           which connects to all sites in the top row.
+         */
+        for (int i = 0; i < N; i += 1) {
+            ufTopOnly.union(i, N * N);
+        }
         size = N;
         openNumber = 0;
     }
@@ -43,11 +47,16 @@ public class Percolation {
         return x * size + y;
     }
 
-    /** open the site (row, col) if it is not open already. */
-    public void open(int row, int col) {
+    /** Check row and col of a site is validate or not. */
+    private void validate(int row, int col) {
         if (!(row >= 0 && row < size) || !(col >= 0 && col < size)) {
             throw new IndexOutOfBoundsException();
         }
+    }
+
+    /** open the site (row, col) if it is not open already. */
+    public void open(int row, int col) {
+        validate(row, col);
         if (!isOpen(row, col)) {
             table[row][col] = true;
             openNumber += 1;
@@ -56,21 +65,25 @@ public class Percolation {
             if (row > 0) {
                 if (isOpen(row - 1, col)) {
                     ufTable.union(n, xyTo1D(row - 1, col));
+                    ufTopOnly.union(n, xyTo1D(row - 1, col));
                 }
             }
             if (row < size - 1) {
                 if (isOpen(row + 1, col)) {
                     ufTable.union(n, xyTo1D(row + 1, col));
+                    ufTopOnly.union(n, xyTo1D(row + 1, col));
                 }
             }
             if (col > 0) {
                 if (isOpen(row, col - 1)) {
                     ufTable.union(n, xyTo1D(row, col - 1));
+                    ufTopOnly.union(n, xyTo1D(row, col - 1));
                 }
             }
             if (col < size - 1) {
                 if (isOpen(row, col + 1)) {
                     ufTable.union(n, xyTo1D(row, col + 1));
+                    ufTopOnly.union(n, xyTo1D(row, col - 1));
                 }
             }
         }
@@ -78,18 +91,21 @@ public class Percolation {
 
     /** Return true if the site (row, col) is open. */
     public boolean isOpen(int row, int col) {
-        if (!(row >= 0 && row < size) || !(col >= 0 && col < size)) {
-            throw new IndexOutOfBoundsException();
-        }
+        validate(row, col);
         return table[row][col];
     }
 
     /** Return true if the site (row, col) is full. */
     public boolean isFull(int row, int col) {
-        if (!(row >= 0 && row < size) || !(col >= 0 && col < size)) {
-            throw new IndexOutOfBoundsException();
-        }
-        return isOpen(row, col) && ufTable.connected(xyTo1D(row, col), size * size);
+        validate(row, col);
+        /* When use ufTopOnly to indicate whether this site is connected to the virtual top site.
+        Because if we use ufTable, there will be backwash. What is backwash?
+        When the system is already percolated and we open some other sites from the bottom, these sites
+        will be full because the virtual top site, virtual bottom site and these sites are all connected.
+        However, these sites shouldn't be full, so we remove the virtual bottom site (using ufTopOnly) to
+        avoid backwash.
+         */
+        return isOpen(row, col) && ufTopOnly.connected(xyTo1D(row, col), size * size);
     }
 
     /** Returns the number of opened sites. */
@@ -128,5 +144,9 @@ public class Percolation {
         System.out.println(p.percolates());
         p.open(3, 2);
         System.out.println(p.percolates());
+
+        System.out.println("### Test backwash");
+        p.open(3, 0);
+        System.out.println(p.isFull(3, 0));
     }
 }
